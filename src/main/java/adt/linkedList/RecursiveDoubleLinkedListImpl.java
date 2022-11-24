bb2e7d700c85b70b714ffa891200991be44ddf2e
jose.arthur.bezerra <jose.arthur.bezerra@ccc.ufcg.edu.br>
2022-11-24 11:11:19 -0300

    implementação do removeFirst em RecursiveDoubleLinkedListImpl.

--- src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
+++ src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
@@ -27,8 +27,9 @@ public class RecursiveDoubleLinkedListImpl<T> extends
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (super.isEmpty()) throw new IndexOutOfBoundsException("Lista vazia");
+		super.setData(super.getNext().getData());
+		super.setNext(super.getNext().getNext());
 	}
 
 	@Override
